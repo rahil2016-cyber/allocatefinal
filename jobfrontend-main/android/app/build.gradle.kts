f33c12a01747017185c283835f1c5f6ba4c48e19
Mohammed Rahil <rahil2016ok@gmail.com>
2026-08-25 15:49:05 +0530

    Update app version to 13

--- jobfrontend-main/android/app/build.gradle.kts
+++ jobfrontend-main/android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
     }
 
     buildTypes {

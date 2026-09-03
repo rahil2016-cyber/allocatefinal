92357b00c40c83871baffecaff9751d844dd48cc
Mohammed Rahil <rahil2016ok@gmail.com>
2026-09-03 20:49:32 +0530

    chore: bump app version to 1.0.15+15

--- jobfrontend-main/android/app/build.gradle.kts
+++ jobfrontend-main/android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 14
-        versionName = "1.0.14"
+        versionCode = 15
+        versionName = "1.0.15"
     }
 
     buildTypes {
